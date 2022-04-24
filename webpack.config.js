a985afcc6cb6111c9cef42ffe2f2d9bae8df91cb
guojunqiang <guojunqiang@daojia-inc.com>
2022-04-24 17:27:05 +0800

    回退处理

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,9 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx.jsx',
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif|webp)$/i,
-                type: "asset/resource"
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)/i,
-                type: "asset/resource"
-            },
-            {
-                test: /\.(csv|tsv)$/i,
-                use: ['csv-loader']
-            },
-            {
-                test: /\.xml$/i,
-                use: ['xml-loader']
-            }
-        ]
-    }
 }
